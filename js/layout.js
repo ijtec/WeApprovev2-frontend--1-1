2de0c0d53460e1778dfb8d44e93b0a024123956e
Okunade sherif <o.d.alsherif@gmail.com>
2023-02-21 16:43:30 +0100

    responsiveness

--- js/layout.js
+++ js/layout.js
@@ -1,6 +1,8 @@
 const dropdownToggle = document.querySelector('.dropdown-toggle');
 const dropdownMenu = document.querySelector('.dropdown-menu');
 const dropdownItems = document.querySelectorAll('.dropdown-item');
+const bars = document.querySelector('.fa-bars');
+const mobileNavbar = document.querySelector('.mobilenav-container');
 
 dropdownToggle.addEventListener('click', function () {
 dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
@@ -11,3 +13,16 @@ item.addEventListener('click', function () {
     dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
 });
 });
+
+
+const toggleNav = () => {
+    if (mobileNavbar.classList == "mobilenav-container") {
+        mobileNavbar.classList.remove("mobilenav-container");
+        mobileNavbar.classList.add("mobilenav-containeractive");
+    } else {
+        mobileNavbar.classList.remove("mobilenav-containeractive");
+        mobileNavbar.classList.add("mobilenav-container");
+    }
+}
+
+
